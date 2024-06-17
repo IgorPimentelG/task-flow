24ef975303980d47442845fe63bde1c765109bca
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2024-06-17 00:33:05 -0300

    :goal_net: feat: add custom exception resolver

--- src/main/java/com/api/taskflow/main/Application.java
+++ src/main/java/com/api/taskflow/main/Application.java
@@ -3,9 +3,13 @@ package com.api.taskflow.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EntityScan("com.api.taskflow.domain.entities")
+@EnableMongoRepositories("com.api.taskflow.infra.repositories")
+@ComponentScan({"com.api.taskflow.infra", "com.api.taskflow.main"})
 public class Application {
 
 	public static void main(String[] args) {
